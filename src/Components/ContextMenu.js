1968a54fab4c7f18a9eeb765b71c997d88a13abb
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2023-01-30 21:13:21 +0530

    context menu styling

--- src/Components/ContextMenu.js
+++ src/Components/ContextMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IoIosArrowForward } from "react-icons/io";
+import { FiGrid } from "react-icons/fi";
 
 
 
@@ -11,11 +12,11 @@ function ContextMenu(props) {
       position: "absolute",
       height:"200px",
       width:"380px",
-      background:"rgba(0,0,0,0.56)",
-      backdropFilter:" blur(20px) saturate(100%)",
+      background:"rgba(0,0,0,0.26)",
+      backdropFilter:" blur(70px) saturate(100%)",
       left:props.x,
       top:props.y,
-      borderRadius:"8px"
+      borderRadius:"8px",
     }
         
   
@@ -23,9 +24,13 @@ function ContextMenu(props) {
 
  
     const itemList=item.map((i)=>{
-      return <div style={{display:"flex",alignItems:"start",justifyContent:"space-between",border:"solid blue 1px"}}>
-        <li style={{listStyle: "none",paddingBottom:"15px"}}>{i}</li>
-        <IoIosArrowForward style={{paddingTop:"3px"}}/>
+      return <div style={{display:"flex",height:"30px",alignItems:"center",justifyContent:"space-between",border:"solid blue 1px",}}>
+        <div style={{display:"flex",alignItems:"end"}}>
+          <FiGrid size={19} style={{paddingTop:"1px",paddingRight:"10px",color:"rgba(255, 255, 255,0.5)"}}/>
+          <li style={{listStyle: "none",fontWeight:"normal",letterSpacing:"0.3px",fontSize:"16px"}}>{i}</li>
+        </div>
+        <IoIosArrowForward style={{color:"rgba(255, 255, 255,0.5)"}}/>
+        
       </div>;
     });
 
@@ -34,8 +39,8 @@ function ContextMenu(props) {
 
     <> 
       <div style={MenuStyle}></div>
-      <div style={{color:"white",left:props.x,top:props.y,position:"absolute",fontSize:"15px",}}>
-        <ul style={{paddingLeft:"20px",width:"350px"}}>
+      <div style={{color:"white",left:props.x,top:props.y-10,position:"absolute",fontSize:"15px",}}>
+        <ul style={{paddingLeft:"20px",paddingTop:"0px",width:"350px"}}>
           {itemList}
         </ul>
       </div>
