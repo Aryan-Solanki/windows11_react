1215b7f7411b1b2127c125bd3460566c8961d0ba
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2023-01-30 21:55:58 +0530

    context menu styling

--- src/Components/ContextMenu.js
+++ src/Components/ContextMenu.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { IoIosArrowForward } from "react-icons/io";
 import { FiGrid } from "react-icons/fi";
+import { IoRefresh } from "react-icons/io5";
+import { TbArrowsSort } from "react-icons/tb";
 
 
 
@@ -8,28 +10,29 @@ function ContextMenu(props) {
 
 
     const MenuStyle={
-      border:"solid rgba(85,85,85,0.4) 1px",
+      border:"solid rgba(85,85,85,0.6) 1px",
       position: "absolute",
-      height:"200px",
+      height:"122px",
       width:"380px",
-      background:"rgba(0,0,0,0.26)",
+      background:"rgba(0,0,0,0.56)",
       backdropFilter:" blur(70px) saturate(100%)",
       left:props.x,
       top:props.y,
       borderRadius:"8px",
+      boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
     }
         
   
-    const item = ["View","Refresh","Undo Delete"];
+    const item = [{title:"View",icon:FiGrid,arrow:true},{title:"Sort by",icon:TbArrowsSort,arrow:true},{title:"Refresh",icon:IoRefresh,arrow:false},];
 
  
     const itemList=item.map((i)=>{
-      return <div style={{display:"flex",height:"30px",alignItems:"center",justifyContent:"space-between",border:"solid blue 1px",}}>
+      return <div style={{display:"flex",height:"35px",alignItems:"center",justifyContent:"space-between",border:"solid blue 1px",}}>
         <div style={{display:"flex",alignItems:"end"}}>
-          <FiGrid size={19} style={{paddingTop:"1px",paddingRight:"10px",color:"rgba(255, 255, 255,0.5)"}}/>
-          <li style={{listStyle: "none",fontWeight:"normal",letterSpacing:"0.3px",fontSize:"16px"}}>{i}</li>
+          <i.icon size={23} style={{paddingTop:"1px",paddingRight:"17px",color:"rgba(255, 255, 255,0.65)"}}/>
+          <li style={{listStyle: "none",fontWeight:"normal",letterSpacing:"0.3px",fontSize:"16px"}}>{i.title}</li>
         </div>
-        <IoIosArrowForward style={{color:"rgba(255, 255, 255,0.5)"}}/>
+        {i.arrow&&<IoIosArrowForward style={{color:"rgba(255, 255, 255,0.7)"}}/>}
         
       </div>;
     });
@@ -39,8 +42,8 @@ function ContextMenu(props) {
 
     <> 
       <div style={MenuStyle}></div>
-      <div style={{color:"white",left:props.x,top:props.y-10,position:"absolute",fontSize:"15px",}}>
-        <ul style={{paddingLeft:"20px",paddingTop:"0px",width:"350px"}}>
+      <div style={{color:"white",left:props.x,top:props.y-8,position:"absolute",fontSize:"15px",}}>
+        <ul style={{paddingLeft:"20px",paddingTop:"0px",width:"340px"}}>
           {itemList}
         </ul>
       </div>
