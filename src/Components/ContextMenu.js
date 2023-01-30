4959eb0638ffcc7b43a592d5e6391d4611a30f18
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2023-01-30 22:34:02 +0530

    hovering done on context menu

--- src/Components/ContextMenu.js
+++ src/Components/ContextMenu.js
@@ -1,13 +1,55 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { IoIosArrowForward } from "react-icons/io";
 import { FiGrid } from "react-icons/fi";
 import { IoRefresh } from "react-icons/io5";
 import { TbArrowsSort } from "react-icons/tb";
 
 
+function MenuTile(props) {
+
+  const [isHover, setIsHover] = useState(false);
+
+
+  const handleMouseEnter = () => {
+      setIsHover(true);
+  };
+  const handleMouseLeave = () => {
+      setIsHover(false);
+  };
+  
+  const MenuTileStyle={
+    padding:"0px 10px",
+    display:"flex",
+    height:"35px",
+    alignItems:"center",
+    justifyContent:"space-between",
+    borderRadius:"5px"
+  }
+
+  const HoverMenuTileStyle={
+    padding:"0px 10px",
+    display:"flex",
+    height:"35px",
+    alignItems:"center",
+    justifyContent:"space-between",
+    background:"rgba(255, 255, 255,0.05)",
+    borderRadius:"5px"
+  }
+
+  return (
+    <div style={isHover?HoverMenuTileStyle:MenuTileStyle}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div style={{display:"flex",alignItems:"end"}}>
+          <props.i.icon size={23} style={{paddingTop:"1px",paddingRight:"17px",color:"rgba(255, 255, 255,0.65)"}}/>
+          <li style={{listStyle: "none",fontWeight:"normal",letterSpacing:"0.3px",fontSize:"16px"}}>{props.i.title}</li>
+        </div>
+        {props.i.arrow&&<IoIosArrowForward style={{color:"rgba(255, 255, 255,0.7)"}}/>}
+      </div>
+  )
+}
+
+
 
 function ContextMenu(props) { 
-
 
     const MenuStyle={
       border:"solid rgba(85,85,85,0.6) 1px",
@@ -20,21 +62,13 @@ function ContextMenu(props) {
       top:props.y,
       borderRadius:"8px",
       boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
-    }
-        
+    }  
   
     const item = [{title:"View",icon:FiGrid,arrow:true},{title:"Sort by",icon:TbArrowsSort,arrow:true},{title:"Refresh",icon:IoRefresh,arrow:false},];
 
  
     const itemList=item.map((i)=>{
-      return <div style={{display:"flex",height:"35px",alignItems:"center",justifyContent:"space-between",border:"solid blue 1px",}}>
-        <div style={{display:"flex",alignItems:"end"}}>
-          <i.icon size={23} style={{paddingTop:"1px",paddingRight:"17px",color:"rgba(255, 255, 255,0.65)"}}/>
-          <li style={{listStyle: "none",fontWeight:"normal",letterSpacing:"0.3px",fontSize:"16px"}}>{i.title}</li>
-        </div>
-        {i.arrow&&<IoIosArrowForward style={{color:"rgba(255, 255, 255,0.7)"}}/>}
-        
-      </div>;
+      return <MenuTile i={i}/>
     });
 
 
@@ -43,7 +77,7 @@ function ContextMenu(props) {
     <> 
       <div style={MenuStyle}></div>
       <div style={{color:"white",left:props.x,top:props.y-8,position:"absolute",fontSize:"15px",}}>
-        <ul style={{paddingLeft:"20px",paddingTop:"0px",width:"340px"}}>
+        <ul style={{paddingLeft:"8px",paddingTop:"0px",width:"364px"}}>
           {itemList}
         </ul>
       </div>
