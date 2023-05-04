974fee002c1e2e0874f644e933a478227cb0f386
Sanjana60 <solankiaryan5@gmail.com>
2023-05-04 13:04:53 +0530

    fixes

--- src/Components/BottomBarRightIcon/BottomBarRightIcon.js
+++ src/Components/BottomBarRightIcon/BottomBarRightIcon.js
@@ -4,6 +4,7 @@ import {useState,useEffect} from 'react';
 import {useSelector,useDispatch } from "react-redux";
 
 import { openSideBarr,closeSideBarr } from "../Slices/isIconClickSlice";
+import sidepane from '../../Asset/sidepane.png';
 
 function BottomBarRightIcon(props) {
 
@@ -35,7 +36,7 @@ function BottomBarRightIcon(props) {
 
 
     return (
-      <div  onClick={isSideBarOpen===true?closeSideBar:openSideBar}  onMouseEnter={mouseEnter} onMouseLeave={mouseExit} style={{background:isHover?"rgba(255, 255, 255,0.8)":"none"}}>
+      <div  onClick={props.i===sidepane?(isSideBarOpen===true?closeSideBar:openSideBar):()=>{}}  onMouseEnter={mouseEnter} onMouseLeave={mouseExit} style={{background:isHover?"rgba(255, 255, 255,0.8)":"none"}}>
         <img src={props.i} alt="home" style={{ height: "20px", padding: "7px 7px"}} />
     </div>
     )
